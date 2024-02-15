3e9e5478331d94f5b3bc347a44ab1b9a7d9f6539
Plaum Toon (s146219) <toon.plaum@student.ap.be>
2024-02-15 20:23:47 +0100

    Added catchPokemon

--- index.ts
+++ index.ts
@@ -8,75 +8,129 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("port", 3000);
 
-
 interface NonDetailedPokemon {
-    name: string,
-    url: string,
+  name: string;
+  url: string;
 }
 
 interface DetailedPokemon {
-    id: number,
-    name: string,
-    types: string[],
-    image: string,
-    height: number,
-    weight: number,
-    maxHP: number,
+  id: number;
+  name: string;
+  types: string[];
+  image: string;
+  height: number;
+  weight: number;
+  maxHP: number;
+  defense: number;
+  attack: number;
 }
 
-let pokemons : DetailedPokemon[];
+let pokemons: DetailedPokemon[] = [];
+let playerPokemons: DetailedPokemon[] = [];
 
-// # deze handler is enkel om testen 
-app.get('/getDataAPI',(req,res)=>{
-    res.type('application/json');
-    res.json(pokemons);
+function randomIntFromInterval(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function catchPokemon(
+  targetPokemon: DetailedPokemon,
+  currentPokemon: DetailedPokemon
+): boolean {
+  const catchPercentage = 100 - targetPokemon.defense + currentPokemon.attack;
+  let caught = false;
+  const random = randomIntFromInterval(1, 100);
+
+  if (random <= catchPercentage) {
+    caught = true;
+  }
+
+  return caught;
+}
+
+// # deze handler is enkel om testen
+app.get("/getDataAPI", (req, res) => {
+  res.type("application/json");
+  res.json(pokemons);
 });
 
-app.get('/', async (req,res)=>{
-    res.render("home",{pokemons})
+app.get("/", async (req, res) => {
+  res.render("home", { pokemons });
 });
 
 app.get("/login", async (req, res) => {
-    res.render("login");
+  res.render("login");
 });
 
 app.get("/signup", async (req, res) => {
-    res.render("signup");
+  res.render("signup");
 });
 
 app.get("/vergelijken", async (req, res) => {
-    res.render("vergelijken",{pokemons});
+  res.render("vergelijken", { pokemons });
 });
 
 app.get("/vechten", async (req, res) => {
-    res.render("vechten",{pokemons});
+  res.render("vechten", { pokemons });
 });
 
 app.get("/mypokemons", async (req, res) => {
-    let playerPokemons : DetailedPokemon[] = [pokemons[1],pokemons[2],pokemons[3],pokemons[4],pokemons[5],pokemons[6],pokemons[7],pokemons[8]]
-    res.render("myPokemons",{playerPokemons});
+  playerPokemons = [
+    pokemons[1],
+    pokemons[2],
+    pokemons[3],
+    pokemons[4],
+    pokemons[5],
+    pokemons[6],
+    pokemons[7],
+    pokemons[8],
+  ];
+  res.render("myPokemons", { playerPokemons });
 });
 
 app.get("/pokemon/info/:pokeId", async (req, res) => {
-    const pokemonId = parseInt(req.params.pokeId);
-    const pokemonFind = pokemons.find(({ id }) => pokemonId === id);
-    res.render("pokemoninfo", {pokemonFind})
-}); 
+  const pokemonId = parseInt(req.params.pokeId);
+  const pokemonFind = pokemons.find(({ id }) => pokemonId === id);
+  res.render("pokemoninfo", { pokemonFind });
+});
 
-app.listen(app.get('port'), async ()=>{
-    const apiResult = await (await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")).json();
-    const promisePerPokemon: Promise<Response>[] = (apiResult.results as NonDetailedPokemon[]).map(({ url }) => fetch(url));
-    const jsons = (await Promise.all(promisePerPokemon)).map((response) => response.json());
-    pokemons = (await Promise.all(jsons)).map((singlePokemon) => {
-        return {
-            id: singlePokemon.id,
-            name: singlePokemon.name,
-            types: singlePokemon.types.map((slotAndType: any) => slotAndType.type.name),
-            image: singlePokemon.sprites.front_default,
-            height: singlePokemon.height,
-            weight: singlePokemon.weight,
-            maxHP: singlePokemon.stats[0].base_stat,
-        }
-    });
-    console.log( '[server] http://localhost:' + app.get('port'));
+app.post("/catch", async (req, res) => {
+  const targetPokemon = req.body.pokemon as DetailedPokemon;
+  const currentPokemon = {} as DetailedPokemon;
+
+  for (let i = 0; i < 3; i++) {
+    let caught = catchPokemon(targetPokemon, currentPokemon);
+
+    if (caught) {
+        playerPokemons.push(targetPokemon)
+        break
+    }
+  }
+});
+
+app.listen(app.get("port"), async () => {
+  const apiResult = await (
+    await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+  ).json();
+  const promisePerPokemon: Promise<Response>[] = (
+    apiResult.results as NonDetailedPokemon[]
+  ).map(({ url }) => fetch(url));
+  const jsons = (await Promise.all(promisePerPokemon)).map((response) =>
+    response.json()
+  );
+  pokemons = (await Promise.all(jsons)).map((singlePokemon) => {
+    return {
+      id: singlePokemon.id,
+      name: singlePokemon.name,
+      types: singlePokemon.types.map(
+        (slotAndType: any) => slotAndType.type.name
+      ),
+      image: singlePokemon.sprites.front_default,
+      height: singlePokemon.height,
+      weight: singlePokemon.weight,
+      maxHP: singlePokemon.stats[0].base_stat,
+      defense: singlePokemon.stats[2].base_stat,
+      attack: singlePokemon.stats[1].base_stat,
+    };
+  });
+  console.log("[server] http://localhost:" + app.get("port"));
 });
