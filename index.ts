e3e96dba12bdbfa0b4032ef0362a208ecbc6042c
De Bie Gilles (s146372) <gilles.debie@student.ap.be>
2024-05-04 12:12:46 +0200

    logout

--- index.ts
+++ index.ts
@@ -205,6 +205,17 @@ app.post("/login", async (req, res) => {
   }
 });
 
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error("Error destroying session:", err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      res.redirect("/login");
+    }
+  });
+});
+
 app.get("/signup", async (req, res) => {
   res.render("signup", { pokemons });
 });
